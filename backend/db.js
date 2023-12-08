167ca42d49f1ee6cf4516c995e913fe22db7d2c8
Nik4Furi <nikhilgurjar2212@gmail.com>
2023-12-08 11:05:33 +0530

    Deploy our app

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 //Connect app with database
-mongoose.connect(process.env.MONGO_URI). 
+const connection = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/user-auths"
+mongoose.connect(connection). 
 then( ()=> console.log('Connection to database')). 
 catch((e) => console.error('Error occured during connection to database ',e))
